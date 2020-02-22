25d4cb2846f488277f72ebd7b2c30d0c262ab885
xTCry <wolt6@mail.ru>
2020-02-22 16:24:41 +0300

    Init repos

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import App from "./containers/App/App";
+
+import './index.css';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
+
+serviceWorker.register();
